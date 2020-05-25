2b763c35de66f502e6055a928727254c2fe6280a
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-25 17:54:57 +0200

    add build in middleware urlencoded and static

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.use(logger);
 app.use(authenticate);
 
